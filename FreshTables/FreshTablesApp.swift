fb4d38c36df04877d913f1458a13a15b6f80be09
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2024-11-08 15:26:47 -0500

    Initial Commit

---
+++ FreshTables/FreshTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshTablesApp.swift
+//  FreshTables
+//
+//  Created by Theo Luciano on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FreshTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FreshTablesTests/FreshTablesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FreshTablesTests.swift
+//  FreshTablesTests
+//
+//  Created by Theo Luciano on 11/8/24.
+//
+
+import Testing
+@testable import FreshTables
+
+struct FreshTablesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
